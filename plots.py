9095d3ea7c7f483af991aa72799eb457721a837c
Antonio Sanna <tonio@localhost.localdomain>
2020-01-28 17:36:13 +0100

    modified some stuff

--- plots.py
+++ plots.py
@@ -2,23 +2,39 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-train = pd.read_csv("resultsTrain.csv", index_col="episode")
+train = pd.read_csv("resultsTest.csv", index_col="episode")
+
+folder='testing'
+
+plt.figure()
 plt.plot(train['operation'].tolist())
-plt.savefig("figures/operation.png")
-plt.title("operation")
+plt.title("OPERATION")
+plt.savefig("figures/"+folder+"/operation.png")
 plt.show()
 
-train = pd.read_csv("resultsTrain.csv", index_col="episode")
-plt.plot(train['operation'].tolist())
-plt.savefig("figures/operation.png")
-plt.title("operation")
+plt.figure()
+plt.plot(train['reward'].tolist())
+plt.title("REWARD")
+plt.axhline(0,color="grey")
+plt.grid(axis='y')
+plt.savefig("figures/"+folder+"/reward.png")
 plt.show()
 
+
+plt.figure()
 x = np.arange(3)
-
 plt.bar(x,height=[train['hold'].tolist()[-1],train['long'].tolist()[-1],train['short'].tolist()[-1]])
-
 plt.xticks(x, ['Hold','Long','Short'])
-plt.savefig("figures/percentages.png")
-plt.title("Percentages")
+plt.title("PERCENTAGES")
+plt.grid(axis='y')
+plt.savefig("figures/"+folder+"/percentages.png")
+plt.show()
+
+
+
+plt.figure()
+plt.plot(train['capital'].tolist())
+plt.title("CAPITAL")
+plt.axhline(0,color="grey")
+plt.savefig("figures/"+folder+"/capital.png")
 plt.show()
